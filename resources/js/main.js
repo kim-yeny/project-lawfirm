266111938b291b3c810e518439d55a75179516ed
kim_yeny <des423@naver.com>
2022-01-14 21:52:45 +0900

    commit

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,19 +2,21 @@
 let scrollBox = $('.scroll');
 
 // 섹션별 상단 좌표값
-const serviceTop = $('#sect-service').position().top - 200;
-const shareTop = $('#sect-share').position().top - 400;
-const consultTop = $('#sect-consult').position().top - 400;
-const socialTop = $('#sect-social').position().top - 200;
-const storyTop = $('#sect-story').position().top;
-console.log(serviceTop, shareTop, consultTop, socialTop, storyTop);
+const serviceTop = $('#service').position().top - 200;
+const shareTop = $('#share').position().top - 200;
+const consultTop = $('#consult').position().top - 200;
+const socialTop = $('#social').position().top - 200;
 
 // 우측 스크롤 박스 > a 태그 클릭 이벤트
 function scrollMove(point) {
     // point에 해당하는 섹션의 좌표값
-    var sectTop = $('#sect-' + point).offset().top;
-    console.log('sectTop: ', sectTop);
-    
+    var sectTop = $('#' + point).offset().top;
+
+    // share, consult 상단에 여유
+    if (point == "share" || point == "consult") {
+        sectTop -= 150;
+    };
+
     // 해당 섹션으로 스크롤 이동
     $('html, body').animate({
         scrollTop: sectTop
@@ -27,19 +29,19 @@ $(function () {
         $(this).prev().toggleClass('on');
     });
 
-    // Reset
-    if ($(window).scrollTop() < serviceTop) scrollBox.children().eq(0).addClass('on');
+    // (Mobile) Menu BTN
+    $('.btn-menu').click(function () {
+        $('.nav').toggleClass('on');
+        $('.btn-menu').toggleClass('fa-times');
+    });
 
+    // Scroll Event
     $(window).scroll(function () {
         // 현재 스크롤 상단의 위치
         var nowTop = $(window).scrollTop();
-        // console.log(nowTop);
-
-        // 스크롤 하단 인식
-        var bottom = $(document).height()-$(window).height();
 
         // 현재 스크롤 위치랑 동일한 위치의 섹션 찾아서 클래스 토글
-        if (nowTop < serviceTop) {
+        if (nowTop < serviceTop && nowTop != 0) {
             scrollBox.children().eq(1).removeClass('on');
             scrollBox.children().eq(0).addClass('on');
         } else if (serviceTop <= nowTop && nowTop < shareTop) {
@@ -54,13 +56,32 @@ $(function () {
             scrollBox.children().eq(2).removeClass('on');
             scrollBox.children().eq(3).addClass('on');
             scrollBox.children().eq(4).removeClass('on');
-        } else if (socialTop <= nowTop && nowTop < bottom) {
+        } else if (socialTop <= nowTop) {
             scrollBox.children().eq(3).removeClass('on');
             scrollBox.children().eq(4).addClass('on');
-            scrollBox.children().eq(5).removeClass('on');
-        } else if (nowTop == bottom) {
-            scrollBox.children().eq(4).removeClass('on');
-            scrollBox.children().eq(5).addClass('on');
         }
+    });
+
+    // Consult form submit
+    $("#consult-form").submit(function () {
+
+        var name = $(this).children("input[name=client-name]");
+        var phone = $(this).children("input[name=client-phone]");
+        var content = $(this).children("textarea[name=client-content]");
+
+        // Validation
+        if (name.val() == "") {
+            alert("이름을 입력해 주세요.");
+            return false;
+        } else if (phone.val() == "") {
+            alert("연락처를 입력해 주세요.");
+            return false;
+        } else if (content.val() == "") {
+            alert("상담하실 내용을 입력해 주세요.");
+            return false;
+        } else {
+            alert("상담 신청이 접수 되었습니다. 감사합니다.")
+            return true;
+        };
     });
 });
